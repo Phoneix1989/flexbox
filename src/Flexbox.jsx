0b892f8ce9854341a0f3f28d0a29a855d80e3ee8
Damilola Adewunmi <damilolaadewunmi@Damilolas-MacBook.local>
2024-04-03 11:27:59 +0100

    flexbox

---
+++ src/Flexbox.jsx
@@ -0,0 +1,34 @@
+import React from "react"
+import "./Flexbox.css"
+function Flexbox() {
+    return(
+        <div className="container_flex"> 
+            <div className="flex_container">
+            <h1 className="tab1">Dam</h1>
+            <h1 className="tab2">Ifeoluwa</h1>
+            <h1 className="tab3">Abisola</h1>
+        </div>
+            
+            <div className="grid_container">
+             <h1 className="grid1">Dam</h1>
+             <h1 className="grid2">Ifeoluwa</h1>
+             <h1 className="grid3">Abisola</h1>
+             <h1 className="grid4">Bimbo</h1> 
+             <h1 className="grid5">segun</h1>
+             <h1 className="grid6">Ife</h1>
+             <h1 className="grid7">sola</h1>
+             <h1 className="grid8">Bide</h1>
+             <h1 className="grid9">Damy</h1>
+             <h1 className="grid10">Oluwa</h1>
+             <h1 className="grid11">Bose</h1>
+             <h1 className="grid12">Bode</h1> 
+             <h1 className="grid13">Dele</h1>
+             <h1 className="grid14">Dara</h1>
+             <h1 className="grid15">Kemi</h1>
+             <h1 className="grid16">Toyin</h1>  
+            </div> 
+        </div>
+    )
+}
+
+export default Flexbox;
